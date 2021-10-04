7b1d8c0c7d9fd1e9e47d2a84a64a0a93fce4de67
SongZihuan <jimsong2010@163.com>
2021-10-04 15:24:10 +0800

    feat: 添加regex对中文的测试

--- test/src/tool_regex.c
+++ test/src/tool_regex.c
@@ -7,10 +7,22 @@ int main() {
     int rc1 = matchRegex("Hello嘿World", rg);
     int rc2 = matchRegex("Nossss", rg);
     freeRegex(rg);
-    if (rc1 == 1 && rc2 == 0) {
-        printf("Success\n");
-        return 0;
-    }
-    printf("Failed: %d, %d\n", rc1, rc2);
-    return 1;
+    if (rc1 != 1 || rc2 != 0) {
+        printf("Failed rg1: %d/1, %d/0\n", rc1, rc2);
+        return 1;
+    } else
+        printf("rg1 success\n");
+
+    af_Regex *rg2 = makeRegex("你|好");
+    int rc3 = matchRegex("你", rg2);
+    int rc4 = matchRegex("Nosssss", rg2);
+    freeRegex(rg2);
+    if (rc3 != 1 || rc4 != 0) {
+        printf("Failed rg2: %d/1, %d/0\n", rc1, rc2);
+        return 1;
+    } else
+        printf("rg2 success\n");
+
+    printf("All success\n");
+    return 0;
 }
